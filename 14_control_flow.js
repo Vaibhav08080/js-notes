fcb6bc6ff143111249f3167aa06401da9ca39c6e
vaibhav <vibhuuu05@gmail.com>
2025-08-09 22:00:18 +0530

    control_flow

---
+++ 14_control_flow.js
@@ -0,0 +1,19 @@
+// is statement
+if (true) {
+
+    
+}
+
+
+// falsy value 
+// false , 0 , -0 , BigInt 0n, " , null , undefined , NaN
+
+
+//NUllish coalecing Operator (??) : null defined
+
+let val1;
+val1 = 5 ?? 10
+console.log(val1);
+
+// ternary operator
+condition ? true: false
